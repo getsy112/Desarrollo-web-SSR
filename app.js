98456f1a0c27ed227e450a68219154c31fe479b8
getsy112 <144243881+getsy112@users.noreply.github.com>
2026-09-22 14:21:53 +0000

    ✨Feat: Enhance app.js Enhance app.js With middleware configuration and error handling comments; update www for debug message; add greet script in package.json; include perrito panzon image

--- app.js
+++ app.js
@@ -1,24 +1,34 @@
+//Funcion para manejar errores en la aplicaicon
 var createError = require('http-errors');
+//importa el framework axpress
 var express = require('express');
+//importa modulos para manejar rutas (path)
 var path = require('path');
+//para kokies
 var cookieParser = require('cookie-parser');
+//restro para saber que pasa en el servidor Morgan
 var logger = require('morgan');
-
+//se importan las rutas de la aplicación
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-
+//crea la aplicación de express
 var app = express();
 
 // view engine setup
+//configura el motor de vistas
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
 
+//configura los midlewaves de la aplicación
 app.use(logger('dev'));
 app.use(express.json());
+
+//configuración de archivos estáticos
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+//registramos las rutas 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
