b0d004917ccbf326e253505a2be17e4103491ded
getsy112 <144243881+getsy112@users.noreply.github.com>
2026-09-25 19:03:39 +0000

    ✨FEAT ES modules update Package.json scripts and enchande app.js with debug logging

--- server/app.js
+++ server/app.js
@@ -1,31 +1,32 @@
 // Funcion para manejar errores en la aplicación
 import createError from 'http-errors';
-
 // Importa el framework Express
 import express from 'express';
-
 // Importa módulos para manejar rutas
 import path, { dirname } from 'node:path';
-
 // Para cookies
 import cookieParser from 'cookie-parser';
-
 // Registrador para saber qué pasa en el servidor
 import logger from 'morgan';
-
+//Importando biblioteca DEBUG
+import createDebug from 'debug'; 
 // Imports para crear __dirname
 import { fileURLToPath } from 'node:url';
-
-// Crear __dirname
+// Crear objeto de debug
+const debug = createDebug('desarrollo-web-ssr:app');
+// Crear las variables __dirname
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 // Se importan las rutas de la aplicación
+
 import indexRouter from './routes/index.js';
+
 import usersRouter from './routes/users.js';
 
 // Crea la aplicación de Express
-const app = express();
+debug(" 🔨Creando Backend")
+var app = express();
 
 // Configura el motor de vistas
 app.set('views', path.join(__dirname, 'views'));
@@ -33,19 +34,17 @@ app.set('view engine', 'hbs');
 
 // Configura los middlewares de la aplicación
 app.use(logger('dev'));
-
 app.use(express.json());
-
 app.use(express.urlencoded({ extended: false }));
-
 app.use(cookieParser());
 
 // Configuración de archivos estáticos
+debug(" 🔨Creando servidor de archivos estáticos")
 app.use(express.static(path.join(__dirname, '..', 'public')));
 
 // Registramos las rutas
+debug(" 🛤️ Registrando rutas")
 app.use('/', indexRouter);
-
 app.use('/users', usersRouter);
 
 // Catch 404 y enviar al manejador de errores
