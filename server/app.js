e2794015391969c2de8e7566361ae02f8e821ea5
getsy112 <144243881+getsy112@users.noreply.github.com>
2026-09-25 18:30:51 +0000

    📶Progress ESM 2026 Migrate to ES modules and update route handing

--- server/app.js
+++ server/app.js
@@ -1,58 +1,68 @@
-//Funcion para manejar errores en la aplicaicon
-//var.createError = require('http-errors');
+// Funcion para manejar errores en la aplicación
 import createError from 'http-errors';
-//importa el framework axpress
+
+// Importa el framework Express
 import express from 'express';
-//var express = require('express');
-//importa modulos para manejar rutas (path)
-import path from 'node:path';
-//var path = require('path');
-//para kokies
+
+// Importa módulos para manejar rutas
+import path, { dirname } from 'node:path';
+
+// Para cookies
 import cookieParser from 'cookie-parser';
-//var cookieParser = require('cookie-parser');
-//restro para saber que pasa en el servidor Morgan
-import logger from 'morgana';
-//var registrador = require('morgan');
 
-//se importan las rutas de la aplicación
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+// Registrador para saber qué pasa en el servidor
+import logger from 'morgan';
 
-//crea la aplicación de express
-var app = express();
+// Imports para crear __dirname
+import { fileURLToPath } from 'node:url';
 
-//configura el motor de vistas
-app.set('views', path.join(_dirname, 'views'));
+// Crear __dirname
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+// Se importan las rutas de la aplicación
+import indexRouter from './routes/index.js';
+import usersRouter from './routes/users.js';
+
+// Crea la aplicación de Express
+const app = express();
+
+// Configura el motor de vistas
+app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
 
-
-//configura los midlewaves de la aplicación
+// Configura los middlewares de la aplicación
 app.use(logger('dev'));
+
 app.use(express.json());
 
-//configuración de archivos estáticos
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname,'..','public')));
 
-//registramos las rutas 
+app.use(cookieParser());
+
+// Configuración de archivos estáticos
+app.use(express.static(path.join(__dirname, '..', 'public')));
+
+// Registramos las rutas
 app.use('/', indexRouter);
+
 app.use('/users', usersRouter);
 
-// catch 404 and forward to error handler
+// Catch 404 y enviar al manejador de errores
 app.use(function(req, res, next) {
   next(createError(404));
 });
 
-// error handler
+// Manejador de errores
 app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
+
+  // Variables locales, solo mostrando error en desarrollo
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
+  // Renderizar página de error
   res.status(err.status || 500);
   res.render('error');
 });
 
-module.exports = app;
+export default app;
